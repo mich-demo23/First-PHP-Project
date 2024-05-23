160f0f139c10a900b5b460626cc51a439f08d0d2
Michael Amao <138993304+mich-demo23@users.noreply.github.com>
2024-05-23 23:19:46 +0100

    Update transactions.php

--- views/transactions.php
+++ views/transactions.php
@@ -3,7 +3,7 @@
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transanction Statement App</title>
+    <title>Transaction Statement App</title>
     </head>
 </html>
 <body>
